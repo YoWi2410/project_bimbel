a71b07a79969b260149d4430aee415e9bd9a42a0
Yohanes Wijaya <yohanes.422020021@civitas.ukrida.ac.id>
2022-07-18 05:26:38 +0700

    init midtrans

--- project/app/Models/User.php
+++ project/app/Models/User.php
@@ -24,6 +24,8 @@ class User extends Authenticatable
         'password',
         'avatar',
         'jenjang',
+        'phone', 
+        'address',
         'is_admin',
         'email_verified_at'
     ];
